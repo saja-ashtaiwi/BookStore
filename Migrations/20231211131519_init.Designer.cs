36fb484100625356ec96a66bcfa816d952e306c1
saja-ashtaiwi <sajaashtaiwi@gmail.com>
2024-12-05 18:13:38 +0300

    20231211131519_init.Designer.cs

--- Migrations/20231211131519_init.Designer.cs
+++ Migrations/20231211131519_init.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using YtBookStore.Models.Domain;
+using BookStore.Models.Domain;
 
 #nullable disable
 
